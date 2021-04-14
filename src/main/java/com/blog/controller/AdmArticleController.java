d49d2efead5efc27e273736d2247e1b41c2b7814
limgaram <garam990317@naver.com>
2021-04-14 22:35:41 +0900

    사용자 페이지 articleController 추가

--- src/main/java/com/blog/controller/AdmArticleController.java
+++ src/main/java/com/blog/controller/AdmArticleController.java
@@ -1,15 +1,22 @@
 package com.blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.blog.dto.Article;
-import com.blog.dto.ResultData;
-import com.blog.util.Util;
+import com.blog.service.ArticleService;
+import com.blog.service.GenFileService;
 
 @Controller
 public class AdmArticleController {
+	@Autowired
+	private ArticleService articleService;
+	@Autowired
+	private GenFileService genFileService;
 
-	
+	@RequestMapping("/adm/article/list")
+	public String showList() {
+		return "adm/article/list";
+	}
+
 }
