41baf0ce1bdf891193bea266d2fbf4d35ca9a74c
limgaram <garam990317@naver.com>
2021-05-03 23:19:08 +0900

    AdmMember 추가

--- src/main/java/com/blog/controller/AdmMemberController.java
+++ src/main/java/com/blog/controller/AdmMemberController.java
@@ -1,5 +1,36 @@
 package com.blog.controller;
 
-public class AdmMemberController {
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.blog.service.Memberservice;
+import com.blog.util.Util;
+
+@Controller
+public class AdmMemberController extends BaseController{
+	@Autowired
+	private Memberservice memberService;
+	
+	@RequestMapping("adm/member/join")
+	public String showJoin() {
+		return "adm/member/join";
+	}
+	
+	@RequestMapping("adm/member/doJoin")
+	@ResponseBody
+	public String doJoin(@RequestParam Map<String, Object> param) {
+		if(param.get("loginId") == null) {
+			return Util.msgAndBack("loginId를 입력해주세요.");
+		}
+		
+		String msg = String.format(null, null);
+		String redirectUrl = Util.ifEmpty(null, null);
+		
+		return Util.msgAndBack(msg, redirectUrl);
+	}
 }
---
+++ src/main/java/com/blog/dto/Member.java
@@ -0,0 +1,24 @@
+package com.blog.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member extends EntityDto{
+	private int id;
+	private String regDate;
+	private String updateDate;
+	private String loginId;
+	@JsonIgnore
+	private String loginPw;
+	private int authLevel;
+	private String name;
+	private String cellphoneNo;
+	private String email;
+	private String extra__thumbImg;
+}
